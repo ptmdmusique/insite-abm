faa03ffcd4f7ffba6a3bdff5703bed0dd0306b9c
Duc Phan Thanh <ptmdmusic@gmail.com>
2020-06-23 10:08:18 -0700

    clean up driver, add ticks concept

--- model_driver.py
+++ model_driver.py
@@ -6,20 +6,19 @@ tick_path = "data/tick0.csv"
 cit_geojson_path = "data/compact-cit-shape.json"
 
 # Load in the data file then pass it into the model
-agent_list = pd.read_csv(tick_path,
-                         usecols=["id", "xcor", "ycor", "own-pref",
-                                  "ideo", "idatt", "pref", "tpreference", "power",
-                                  "prox", "proximity", "salience", "im", "own-eu"])
 """Note:
     im - influence message
     tpreference - salience preference
     pref - preference
     own-pref - ?
 """
+agent_list = pd.read_csv(tick_path)
 
 # Load geojson files
 with open(cit_geojson_path) as jsonFile:
     cit_geojson = json.load(jsonFile)
+
 # Load in and run the model
-empty_model = NetLogoModel(agent_list.to_dict(orient='records'), cit_geojson)
-empty_model.step()
+netlogo_model = NetLogoModel(agent_list, cit_geojson, verbose=True)
+for tick in range(26):
+    netlogo_model.step(tick)
